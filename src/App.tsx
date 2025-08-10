ccc541344eb1d179ec5e276986fdff7fef3e6cd0
Sakthi Ram <72368668+sakthiram17@users.noreply.github.com>
2025-08-10 13:53:38 +0530

    added sliderinput component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Button from "./components/ui/Button/Button";
+import SliderInput from "./components/ui/Inputs/SliderInput";
 
 function App() {
   return (
@@ -12,6 +13,17 @@ function App() {
           gap: "20px",
         }}
       >
+        <SliderInput
+          value={0.5}
+          errorMessage="limit exceeded"
+          validationFunction={() => true}
+          showMinMax={true}
+          onChange={() => {}}
+          min={0}
+          step={0.1}
+          max={10}
+          label="Winding factor"
+        ></SliderInput>
         <Button label="Get cores"></Button>
         <Button label="Get cores" styleType="secondary"></Button>
         <Button label="Get cores" styleType="danger"></Button>
---
+++ src/components/ui/Inputs/SliderInput.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "./SliderInput.css";
+
+type SliderInputProps = {
+  label?: string;
+  min: number;
+  max: number;
+  step?: number;
+  value: number;
+  onChange: (value: number) => void;
+  disabled?: boolean;
+  showMinMax?: boolean;
+  showValue?: boolean;
+  validationFunction?: (value: number) => boolean;
+  errorMessage?: string;
+};
+
+const SliderInput: React.FC<SliderInputProps> = ({
+  label,
+  min,
+  max,
+  step = 1,
+  value,
+  onChange,
+  disabled = false,
+  showMinMax = false,
+  showValue = true,
+  errorMessage = "",
+  validationFunction,
+}) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(Number(e.target.value));
+    setIsTouched(true);
+  };
+  const [isTouched, setIsTouched] = useState(false);
+
+  return (
+    <div className="slider-input-container">
+      <div className="slider-input">
+        {showMinMax && <div className="slider-min">{min}</div>}
+        <input
+          type="range"
+          min={min}
+          max={max}
+          step={step}
+          onChange={handleChange}
+          disabled={disabled}
+          className="slider-input-range"
+        />
+        {showMinMax && <div className="slider-max">{max}</div>}
+      </div>
+      <div className="slider-label-container">
+        {label && <label className="slider-input-label">{label}</label>}
+        {showValue && <span className="slider-input-value">{value}</span>}
+      </div>
+      {isTouched && validationFunction && !validationFunction(value) && (
+        <div className="slider-input-error">{errorMessage}</div>
+      )}
+    </div>
+  );
+};
+
+export default SliderInput;
